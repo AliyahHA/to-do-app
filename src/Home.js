eef9b8d36bc60ca463c064d5c6fa76c5782945db
AliyahHA <a-liyahahmed@hotmail.com>
2024-03-15 18:12:55 +0200

    added click events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
 const Home = () => {
-    return (  
-        <div className="home">
-            <h2>Home page</h2>
-        </div>
-    );
-}
- 
+  const handleClick = (e) => {
+    console.log("hello, ninjas", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  };
+
+  return (
+    <div className="home">
+      <h2>Home page</h2>
+      <button onClick={handleClick}>Click me </button>
+      <button onClick={(e) => handleClickAgain("Aliyah", e)}>
+        Click me again
+      </button>
+    </div>
+  );
+};
+
 export default Home;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,21 @@
 const Navbar = () => {
   return (
-    <nav className="Navbar">
+    <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
         <a
           href="/create"
+          /*
+          This is how we can do inlign styling and create a dynamic value
           style={{
             color: "white",
             backgroundColor: "#f1356d",
             borderRadius: "8px",
-          }}>
-          New Blog</a>
+          }}*/
+        >
+          New Blog
+        </a>
       </div>
     </nav>
   );
